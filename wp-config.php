9ae6f3ea748a92c9332490c197f01b32ff3a22f6
Rance Aaron <ranceaaron941@gmail.com>
2024-09-11 17:47:32 +0000

    demo for Brian

--- wp-config.php
+++ wp-config.php
@@ -83,7 +83,7 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+//This is Rance and Brian
 
 /* That's all, stop editing! Happy publishing. */
 
