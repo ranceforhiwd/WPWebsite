14bae207ae04f4ebf5bac386f99bd2d65d9171b5
Rance Aaron <ranceaaron941@gmail.com>
2025-02-18 17:51:08 +0000

    removed unused service references

--- ng/sdkObjects.php
+++ ng/sdkObjects.php
@@ -3,9 +3,8 @@ require 'vendor/autoload.php';
  
 use Aws\S3\S3Client;
 use Aws\DynamoDb\DynamoDbClient;
-use Aws\Ecs\EcsClient;
+
 use Aws\Exception\AwsException;
-use Aws\Sqs\SqsClient;
  
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
